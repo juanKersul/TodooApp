c54fe6aa50e5f8617eb2418cd1a8f459762b6f99
juanKersul <juan.manuel.kersul@gmail.com>
2023-11-12 20:58:06 -0300

    agregado readme

--- backend/app.js
+++ backend/app.js
@@ -7,10 +7,12 @@ const middleware = require("./utils/middleware");
 const logger = require("./utils/logger");
 
 app.use(cors());
+app.use(express.static('dist'));
 app.use(express.json());
 app.use(middleware.requestLogger);
 app.use("/api/todoo", todooRouter);
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
+
 
 module.exports = app;
